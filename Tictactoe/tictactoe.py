5d81200a5f8748a500456cb125a688a076526f5b
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2023-10-17 11:24:47 +0400

    Update winner tictactoe.py

--- Tictactoe/tictactoe.py
+++ Tictactoe/tictactoe.py
@@ -59,7 +59,24 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for i in range(3):
+        # Check rows
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] in ['X', 'O']:
+            return board[i][0]
+        
+        # Check columns
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] in ['X', 'O']:
+            return board[0][i]
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] in ['X', 'O']:
+        return board[0][0]
+    
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] in ['X', 'O']:
+        return board[0][2]
+
+    return None
+
 
 
 def terminal(board):
