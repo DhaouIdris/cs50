fa5ce52997ae6f0b311655951a76e17576cb8ce3
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2023-10-19 14:01:24 +0400

    Solving all puzzle.py

--- knights/puzzle.py
+++ knights/puzzle.py
@@ -12,21 +12,33 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Biconditional(AKnave, Not(AKnight)),
+    # A says "I am both a knight and a knave."
+    Implication(And(AKnave, AKnight), AKnight),
+    Implication(Not(And(AKnave, AKnight)), AKnave)
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    Biconditional(AKnave, Not(AKnight)),
+    Biconditional(BKnave, Not(BKnight)),
+    # A says "We are both knaves."
+    Implication(And(AKnave,BKnave), AKnight),
+    Implication(Not(And(AKnave,BKnave)), AKnave)
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    Biconditional(AKnave, Not(AKnight)),
+    Biconditional(BKnave, Not(BKnight)),
+    # A says "We are the same kind."
+    Biconditional(Or(And(AKnight, BKnight), And(AKnave, BKnave)), AKnight),
+    # B says "We are of different kinds."
+    Biconditional(Or(And(AKnight, BKnave), And(AKnave, BKnight)), BKnight),
 )
 
 # Puzzle 3
@@ -35,10 +47,25 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    Biconditional(AKnave, Not(AKnight)),
+    Biconditional(BKnave, Not(BKnight)),
+    Biconditional(CKnave, Not(CKnight)),
+    # A says either "I am a knight." or "I am a knave.", but you don't know which.
+    Implication(AKnight, Or(AKnight, AKnave)),
+    Implication(AKnave, Not(Or(AKnave, AKnight))),
+    # B says "A said 'I am a knave'."
+    Implication(BKnave, Or(Not(AKnight), Or(AKnight, AKnave))),
+    Implication(BKnight, Or(Not(AKnave), Not(Or(AKnave, AKnight)))),
+    # B says "C is a knave."
+    Implication(Not(CKnave), BKnave),
+    # C says "A is a knight."
+    Biconditional(AKnight, CKnight)
+    
 )
 
-
+ #   Or(AKnight, AKnave),
+  #  Or(BKnight, BKnave),
+   # Or(CKnight, CKnave),
 def main():
     symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
     puzzles = [
